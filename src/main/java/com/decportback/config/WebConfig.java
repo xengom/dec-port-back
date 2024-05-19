db3bfcde30927871e5c29da976af009d07c3eec9
DeC <xengom@gmail.com>
2024-05-20 02:55:44 +0900

    add WebConfiguration

---
+++ src/main/java/com/decportback/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.decportback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS 설정, 허용되지 않은 Origin은 API에 접근 불가
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
